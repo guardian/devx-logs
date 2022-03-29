17b9d3c837f805c607fb25e805af6780d15562be
Nicolas Long <nicolas.long@theguardian.com>
2022-03-29 13:36:58 +0100

    Fetch tags from config file or arg
    This is to work better with:
    
    https://github.com/guardian/instance-tag-discovery
    
    As part of this commit, the fluentbit config has also been
    simplified to just cloud-init for now.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json"
 	"fmt"
 	"os"
 	"sort"
@@ -12,6 +13,8 @@ import (
 
 //go:embed fluentbit/fluentbit.conf
 var fluentbitConfig string
+
+const jsonConfigPath = "/etc/config/tags.json"
 
 func main() {
 	rootCmd := RootCmd()
@@ -31,11 +34,8 @@ func RootCmd() *cobra.Command {
 		Short: "devx-logs outputs a Fluentbit config appropriate for Guardian EC2 applications.",
 		Long:  `devx-logs outputs a Fluentbit config appropriate for Guardian EC2 applications.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			tags := map[string]string{}
-			for _, tag := range strings.Split(tagsArg, ",") {
-				kv := strings.Split(tag, "=")
-				tags[kv[0]] = kv[1]
-			}
+			tags, err := getTags(jsonConfigPath, tagsArg)
+			check(err, "tags not found")
 
 			placeholders := map[string]string{
 				"KINESIS_STREAM": kinesisStreamName,
@@ -53,10 +53,34 @@ func RootCmd() *cobra.Command {
 	rootCmd.Flags().StringVar(&systemdUnit, "systemdUnit", "", "Set to the name of your app's systemd service. I.e. 'name' from [name].service")
 	rootCmd.MarkFlagRequired("systemdUnits")
 
-	rootCmd.Flags().StringVar(&tagsArg, "tags", "", "Set a comma-separated list of Key=Value pairs, to be included on log records. At the least, this should include App, Stack, and Stage. Eg. 'App=foo,Stage=PROD,Stack=bar'.")
-	rootCmd.MarkFlagRequired("tags")
+	rootCmd.Flags().StringVar(&tagsArg, "tags", "", "Set a comma-separated list of Key=Value pairs, to be included on log records. At the least, this should include App, Stack, and Stage. Eg. 'App=foo,Stage=PROD,Stack=bar'. If empty, tags will be sourced from /etc/config/tags.json (see the cdk-base Amigo role).")
 
 	return rootCmd
+}
+
+func getTags(jsonConfigPath string, tagsArg string) (map[string]string, error) {
+	tags := map[string]string{}
+
+	if tagsArg == "" { // lookup from config file
+		raw, err := os.ReadFile(jsonConfigPath)
+		if err != nil {
+			return tags, fmt.Errorf("unable to read tags from %s: %w", jsonConfigPath, err)
+		}
+
+		err = json.Unmarshal(raw, &tags)
+		if err != nil {
+			return tags, fmt.Errorf("unable to unmarshal JSON config from %s: %w", jsonConfigPath, err)
+		}
+
+		return tags, nil
+	}
+
+	for _, tag := range strings.Split(tagsArg, ",") {
+		kv := strings.Split(tag, "=")
+		tags[kv[0]] = kv[1]
+	}
+
+	return tags, nil
 }
 
 func replaceReplaceholders(config string, info map[string]string, tags map[string]string) string {
@@ -75,7 +99,7 @@ func replaceReplaceholders(config string, info map[string]string, tags map[strin
 	// here (partly for tests).
 	sort.StringSlice(addTags).Sort()
 
-	updated = strings.ReplaceAll(updated, "{{TAGS}}", strings.Join(addTags, "\n  "))
+	updated = strings.ReplaceAll(updated, "{{TAGS}}", strings.Join(addTags, "\n    "))
 
 	return updated
 }
