9ae30e93940dbd838e9daca07c6d7c4ff3065444
akash1810 <akash1810@users.noreply.github.com>
2025-08-13 08:19:13 +0100

    refactor(cli): Extract string interpolation to function

--- cli/elk.ts
+++ cli/elk.ts
@@ -5,22 +5,26 @@ function wrapString(str: string): string {
   return `'${str}'`;
 }
 
+function matchPhraseQuery(field: string, value: string) {
+  return `(query:(match_phrase:(${wrapString(field)}:${wrapString(value)})))`;
+}
+
 export function getLink(
   space: string,
-  filters: Record<string, string>,
+  phraseFilters: Record<string, string>,
   columns: string[] = [],
 ): string {
-  const kibanaFilters = Object.entries(filters).map(([key, value]) => {
-    return `(query:(match_phrase:(${wrapString(key)}:${wrapString(value)})))`;
-  });
+  const phraseQueries = Object.entries(phraseFilters).map(([key, value]) =>
+    matchPhraseQuery(key, value)
+  );
 
   // The `#/` at the end is important for Kibana to correctly parse the query string
   // The `URL` object moves this to the end of the string, which breaks the link.
   const base = `https://logs.gutools.co.uk/s/${space}/app/discover#/`;
 
   const query = {
-    ...(kibanaFilters.length > 0 && {
-      _g: `(filters:!(${kibanaFilters.join(",")}))`,
+    ...(phraseQueries.length > 0 && {
+      _g: `(filters:!(${phraseQueries.join(",")}))`,
     }),
     ...(columns.length > 0 && {
       _a: `(columns:!(${columns.map(wrapString).join(",")}))`,
