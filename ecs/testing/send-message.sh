955c35a1e07107d6c3a57396692c1fbeb883773c
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-08-19 10:40:28 +0100

    Remove python testing files

--- ecs/testing/send-message.sh
+++ ecs/testing/send-message.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-python3 -m venv .venv
-source .venv/bin/activate
-python3 -m pip install -r requirements.txt
 MESSAGE="${1:-testing}"
-python3 send-log.py "{\"hello\":\"$MESSAGE\",\"test\":true}"
+go run send-log.go --field log "{\"hello\":\"$MESSAGE\",\"test\":true}"
